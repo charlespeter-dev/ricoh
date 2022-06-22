b201a017ed6439ebde38a50cb12f9ca9cf41fb75
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-06-22 09:38:53 +0800

    adde form template placeholder

---
+++ form/src/js/customer-nomination.js
@@ -0,0 +1,13 @@
+
+
+$(() => {
+
+    $('#customer-nomination').multiselect({
+        templates: {
+            button: '<div class="multiselect dropdown-toggle" data-bs-toggle="dropdown"><input type="text" value="please select" disabled></div>'
+        },
+    });
+
+    
+});
+
