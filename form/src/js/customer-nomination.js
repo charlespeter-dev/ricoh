cfb48974d7d90e4f8a8bba7d1141c6723cc378d2
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-06-23 14:43:04 +0800

    refined form with JS

--- form/src/js/customer-nomination.js
+++ form/src/js/customer-nomination.js
@@ -1,13 +1,50 @@
 
-
 $(() => {
 
-    $('#customer-nomination').multiselect({
+    /**
+     * multi selection helper
+     */
+
+    $('#area-of-expertise, #value-propositions').multiselect({
         templates: {
-            button: '<div class="multiselect dropdown-toggle" data-bs-toggle="dropdown"><input type="text" value="please select" disabled></div>'
+            button: '<div class="multiselect dropdown-toggle form-select" data-bs-toggle="dropdown">&mdash; Please Select &mdash;</div>'
         },
+        onChange: (option, checked) => {
+            let id = option[0].parentElement.id;
+            let selectedOptions = $(`#${id} option:selected`);
+            let ul = $(`#${id}`).parent().siblings('ul');
+
+            if (selectedOptions.length > 0) {
+
+                let tpl = '';
+                selectedOptions.each((i, el) => {
+                    tpl += `<li class="list-group-item active">${el.value}</li>`;
+                });
+
+                ul.html(tpl);
+            }
+            else {
+                ul.html('');
+            }
+        }
     });
 
-    
+    /**
+     * show/hide 'other' option value
+     */
+
+    $(document).on('change', '#account-type, #industry', (e) => {
+
+        let el = e.target;
+        let ffloat = $(el).siblings('.form-floating');
+
+        if (el.value.toLowerCase() == 'other') {
+            ffloat.removeClass('d-none');
+        }
+        else {
+            ffloat.addClass('d-none');
+            ffloat.find('input').val('')
+        }
+    });
 });
 
