eb6a6eb4168d91d381c3fc64afe55f49d412bc3e
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-11-09 19:36:39 +0800

    Subpage 6 IoT ready

---
+++ web/src/build.php
@@ -0,0 +1,20 @@
+<?php
+
+$htmls = glob('*.html');
+$out = [];
+
+foreach ($htmls as $k => $html) {
+    if (strpos($html, '.html') !== false) {
+        $foldername = str_replace(".html", '', $html);
+        $cmd = sprintf("npx parcel build %s --dist-dir ./build/%s --no-optimize --no-source-maps --public-url ./ 2>&1", $html, $foldername);
+
+        if (isset($argv[1]) && $argv[1] == 'all') {
+            exec($cmd, $out);
+        } elseif (!isset($argv[1]) && $foldername == 'main4') {
+            exec($cmd, $out);
+            break;
+        }
+    }
+}
+
+print_r($out);
