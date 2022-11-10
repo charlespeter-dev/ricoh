9a94f768a81d814584f830ee3449da03a46f367e
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-11-10 13:42:40 +0800

    Subpage 9 Mobile App ready

--- phase2/src/build.php
+++ phase2/src/build.php
@@ -10,7 +10,7 @@ foreach ($htmls as $k => $html) {
 
         if (isset($argv[1]) && $argv[1] == 'all') {
             exec($cmd, $out);
-        } elseif (!isset($argv[1]) && in_array($foldername, ['subpage-6-iot', 'subpage-7-digital-signage', 'subpage-8-visitor-management'])) {
+        } elseif (!isset($argv[1]) && in_array($foldername, ['subpage-9-mobile-app'])) {
             exec($cmd, $out);
         }
     }
