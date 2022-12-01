4befed9c0403d309c464165d392b331d08f90405
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-01 14:35:34 +0800

    updated split-text-w-bg component

--- phase2/src/build.php
+++ phase2/src/build.php
@@ -10,7 +10,7 @@ foreach ($htmls as $k => $html) {
 
         if (isset($argv[1]) && $argv[1] == 'all') {
             exec($cmd, $out);
-        } elseif (!isset($argv[1]) && in_array($foldername, ['lp-callout-percentage-single'])) {
+        } elseif (!isset($argv[1]) && in_array($foldername, ['split-text-w-bg'])) {
             exec($cmd, $out);
         }
     }
