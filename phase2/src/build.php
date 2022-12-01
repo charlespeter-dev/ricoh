7351126030a19674c3bc4e5572fc71821866048a
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-01 16:13:53 +0800

    updated phase2 components: removed duplicate H1

--- phase2/src/build.php
+++ phase2/src/build.php
@@ -10,7 +10,7 @@ foreach ($htmls as $k => $html) {
 
         if (isset($argv[1]) && $argv[1] == 'all') {
             exec($cmd, $out);
-        } elseif (!isset($argv[1]) && in_array($foldername, ['split-text-w-bg'])) {
+        } elseif (!isset($argv[1]) && in_array($foldername, ['card-3-columns', 'card-why-ricoh', 'split-width-hero'])) {
             exec($cmd, $out);
         }
     }
