5209c3be2badcc54aa40b3aae6aac708c8142f62
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-02 17:52:21 +0800

    OpCo HK Hybrid Workplace

--- OpCos_HK/build.php
+++ OpCos_HK/build.php
@@ -10,7 +10,7 @@ foreach ($htmls as $k => $html) {
 
         if (isset($argv[1]) && $argv[1] == 'all') {
             exec($cmd, $out);
-        } elseif (!isset($argv[1]) && in_array($foldername, ['main-144', 'main-145'])) {
+        } elseif (!isset($argv[1]) && in_array($foldername, ['main-137', 'main-138'])) {
             exec($cmd, $out);
         }
     }
