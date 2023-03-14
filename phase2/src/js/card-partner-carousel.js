e01b80b9b6472f1bdf2a4f94ec5f438ada215765
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-03-14 12:13:12 +0800

    new main page ready

--- phase2/src/js/card-partner-carousel.js
+++ phase2/src/js/card-partner-carousel.js
@@ -1,18 +1,22 @@
 import "../../../node_modules/bootstrap/js/src/carousel";
 
+const carousels = document.querySelectorAll('.carousel');
 
-let items = document.querySelectorAll('.carousel .carousel-item')
+carousels.forEach(function (el) {
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
+    const items = el.querySelectorAll('.carousel-item');
+
+    items.forEach(function (el) {
+        const minPerSlide = 4;
+        let next = el.nextElementSibling;
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0];
+            }
+            let cloneChild = next.cloneNode(true);
+            el.appendChild(cloneChild.children[0]);
+            next = next.nextElementSibling;
         }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+    });
+});
