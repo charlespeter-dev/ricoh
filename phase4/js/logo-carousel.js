9030c88d5310b2458f64e0c1eeb2b1686e07b584
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-04-13 11:25:24 +0800

    custom logo carousel

---
+++ phase4/js/logo-carousel.js
@@ -0,0 +1,20 @@
+const carousels = document.querySelectorAll('.twox-partner-logo-carousel');
+
+carousels.forEach(function (el) {
+
+    const items = el.querySelectorAll('.carousel-item');
+
+    items.forEach(function (el) {
+        const minPerSlide = 4;
+        let next = el.nextElementSibling;
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0];
+            }
+            let cloneChild = next.cloneNode(true);
+            el.appendChild(cloneChild.children[0]);
+            next = next.nextElementSibling;
+        }
+    });
+});
