1a485cc2d5b6baf6a0fb283f42978906556223a0
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-04-18 15:50:53 +0800

    rebuilt html

--- phase4/build.php
+++ phase4/build.php
@@ -10,7 +10,7 @@ foreach ($htmls as $k => $html) {
 
         if (isset($argv[1]) && $argv[1] == 'all') {
             exec($cmd, $out);
-        } elseif (!isset($argv[1]) && in_array($foldername, ['main-218'])) {
+        } elseif (!isset($argv[1]) && in_array($foldername, ['main-218', 'main-227', 'main-234', 'main-257'])) {
             exec($cmd, $out);
         }
     }
