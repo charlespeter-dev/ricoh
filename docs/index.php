de3aec2ba06181a74dfa6d7ab61abac17022bc73
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2024-05-02 13:17:31 +0800

    Phase 6: Main 424 ready

--- docs/index.php
+++ docs/index.php
@@ -167,9 +167,7 @@ require_once 'functions.php';
     </main>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-        crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"></script>
     <script>
         window.addEventListener('DOMContentLoaded', function () {
