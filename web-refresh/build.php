61eb94f541b399ee22dc387d182d995414858782
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2025-03-14 15:25:58 +0800

    Services Listings 01 ready

---
+++ web-refresh/build.php
@@ -0,0 +1,10 @@
+<?php
+
+$components = glob('components/*.php');
+
+foreach ($components as $component) {
+    $php_filename = basename($component);
+    $raw_filename = pathinfo(basename($component), PATHINFO_FILENAME);
+    $cmd = sprintf('wget "https://ricoh.test/%s" -O "html/%s.html"', $php_filename, $raw_filename);
+    exec($cmd);
+}
